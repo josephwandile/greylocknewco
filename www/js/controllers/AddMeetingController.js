778df64aefa3e30292683f41ceccec4ad807c6cd
Joseph Kahn <josephkahn@Josephs-MacBook-Pro.local>
2015-06-28 02:01:21 -0700

    all controllers integrated with addition forms

--- www/js/controllers/AddMeetingController.js
+++ www/js/controllers/AddMeetingController.js
@@ -1,49 +1,29 @@
 var AddMeetingController = angular.module('AddMeetingController', []);
 
-AddMeetingController.controller('AddMeetingCtrl', ['$scope', 'ParseService', function($scope, ParseService) {
+AddMeetingController.controller('AddMeetingCtrl', ['$scope','$location', 'ParseService', function($scope, $location, ParseService) {
     console.log('Controller Activated');
-
-    console.log('Currently adding a meeting to user: ', ParseService.current_contact_id);
-    var current_contact_id = ParseService.current_contact_id;
 
     console.log('Currently updating meeting with ID ', ParseService.current_meeting_id);
     var current_meeting_id = ParseService.current_meeting_id;
 
-    var excited_by = 'My interest in AI';
-    var laugh = 'Putin';
-    var suggestion = 'Read Paul Graham\'s Essays';
-    var suggested_contact = 'Elon Musk';
-    var intro = 'Yes';
-    var suggest_read = 'Yes';
-    var suggested_to_read = 'The Lean Startup';
-    var advice = 'Find something you\'re not passionate about';
-    var instructions = 'Email me in a week';
-    var meeting_note = 'He\'s an atheist';
+    $scope.questions = ParseService.getQuestions(50, 59);
+    $scope.input = {};
 
-    var payload = {
-        'excited_by': excited_by,
-        'laugh': laugh,
-        'suggestion': suggestion,
-        'suggested_contact': suggested_contact,
-        'intro': intro,
-        'suggest_read': suggest_read,
-        'suggested_to_read': suggested_to_read,
-        'advice': advice,
-        'instructions': instructions,
-        'meeting_note': meeting_note
-    }
+    $scope.submitForm = function() {
 
-    debugger;
-    var authPromise = ParseService.updateMeeting(current_meeting_id, {
-        'data': JSON.stringify(payload)
-    })
+        var payload = $scope.input; 
 
-    authPromise.success(function(data) {
-        debugger;
+        var authPromise = ParseService.updateMeeting(current_meeting_id, {
+            'data': JSON.stringify(payload)
+        })
 
-    }).error(function(data, status) {
-        console.log(status);
-    });
+        authPromise.success(function(data) {
 
-  $scope.questions = ParseService.getQuestions(50, 59);
+        	// Route to recently created meeting
+        	$location.path('tab/meetings/:' + current_meeting_id);
+
+        }).error(function(data, status) {
+            console.log(status);
+        });
+    };
 }]);
--- www/js/controllers/AddProfileController.js
+++ www/js/controllers/AddProfileController.js
@@ -1,9 +1,29 @@
 var AddProfileController = angular.module('AddProfileController', []);
 
-AddProfileController.controller('AddProfileCtrl', ['$scope', 'ParseService', function($scope, ParseService) {
-  console.log('Controller Activated');
+AddProfileController.controller('AddProfileCtrl', ['$scope', 'ParseService' '$location', function($scope, ParseService, $location) {
+    console.log('Controller Activated');
 
-  console.log('Currently updating profile of user ID: ', ParseService.current_contact_id)
+    console.log('Currently updating profile of user ID: ', ParseService.current_contact_id)
+    var current_contact_id = ParseService.current_contact_id;
 
-  $scope.questions = ParseService.getQuestions(4, 11);
+    $scope.questions = ParseService.getQuestions(4, 11);
+    $scope.input = {};
+
+    $scope.submitForm = function() {
+
+        var payload = $scope.input;
+
+        var authPromise = ParseService.updateMeeting(current_contact_id, {
+            'data': JSON.stringify(payload)
+        });
+
+        authPromise.success(function(data) {
+
+        	// Profile created; now add meeting details
+        	$location.path('tab/add/meeting');
+
+        }).error(function(data, status) {
+            console.log(status);
+        });
+    };
 }]);
