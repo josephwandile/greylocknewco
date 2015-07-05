4b330774114b9d7a54758a03a64cef795be8a707
Neel Mehta <neelmehta@college.harvard.edu>
2015-07-04 17:19:42 -0700

    Only overwrite user info if no user exists

--- src/js/services/AccountFactory.js
+++ src/js/services/AccountFactory.js
@@ -1,6 +1,6 @@
 var AccountFactory = angular.module('AccountFactory', [])
 
-AccountFactory.factory('AccountFactory', ['$firebaseAuth', ($firebaseAuth) => {
+AccountFactory.factory('AccountFactory', ['$firebaseAuth', '$firebaseObject', ($firebaseAuth, $firebaseObject) => {
 
     let AccountFactory = {};
 
@@ -16,13 +16,22 @@ AccountFactory.factory('AccountFactory', ['$firebaseAuth', ($firebaseAuth) => {
             AccountFactory.authData = data;
 
             let googleInfo = AccountFactory.authData.google.cachedUserProfile;
-            let newUser = new Firebase('https://201gc.firebaseio.com/users/' +
-                AccountFactory.authData.uid);
 
-            newUser.set({
-                first_name: googleInfo.given_name,
-                last_name: googleInfo.family_name,
-                picture: googleInfo.picture
+            let userRef = new Firebase('https://201gc.firebaseio.com/users/' +
+                AccountFactory.authData.uid);
+            // create a synchronized version
+            AccountFactory.user = $firebaseObject(userRef);
+
+            AccountFactory.user.$loaded().then(() => {
+                // initialize user here if they don't already exist
+                let user = AccountFactory.user;
+
+                if (!user.first_name) {
+                    user.first_name = googleInfo.given_name;
+                    user.last_name = googleInfo.family_name;
+                    user.picture = googleInfo.picture;
+                    user.$save();
+                }
             });
         }
     });
@@ -31,13 +40,18 @@ AccountFactory.factory('AccountFactory', ['$firebaseAuth', ($firebaseAuth) => {
     AccountFactory.logIn = () =>
         AccountFactory.auth.$authWithOAuthPopup('google');
     AccountFactory.logOut = () => {
-        AccountFactory.auth.$unauth();
-        AccountFactory.authData = null;
-    }
-    // check if user is logged in
+            AccountFactory.auth.$unauth();
+            AccountFactory.authData = null;
+        }
+        // check if user is logged in
     AccountFactory.isLoggedIn = () => !!AccountFactory.authData;
     // get user info
     AccountFactory.getAuthData = () => AccountFactory.authData;
+
+    // public-accessible fields:
+    // - user
+    // - auth
+    // - authData
 
     return AccountFactory;
 
