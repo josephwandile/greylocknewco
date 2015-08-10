44b8f46edbbf17bcd8c8f5c26aba3e96c4dfd622
Joseph Kahn <josephkahn@college.harvard.edu>
2015-08-09 21:56:50 -0700

    adding additional logic

--- src/js/controllers/AddController.js
+++ src/js/controllers/AddController.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var AddController = angular.module('AddController', []);
+let AddController = angular.module('AddController', []);
 
 AddController.controller('AddCtrl', ['$scope', '$location', 'ParseService', 'AccountFactory', function($scope, $location, ParseService, AccountFactory) {
 
@@ -10,19 +10,19 @@ AddController.controller('AddCtrl', ['$scope', '$location', 'ParseService', 'Acc
 
   $scope.submitForm = function() {
 
-    // var contacts = [];
+    // let contacts = [];
 
-    var payload = ParseService.trimPayload($scope.input);
+    let payload = ParseService.trimPayload($scope.input);
 
-    var first_name = payload['first_name'];
-    var last_name = payload['last_name'];
-    var location = payload['location'];
+    let firstName = payload.first_name;
+    let lastName = payload.last_name;
+    let location = payload.location;
 
-    var met_at = $scope.input['met_at'];
-    var type = payload['type'].toUpperCase();
+    let metAt = $scope.input.met_at;
+    let type = payload.type.toUpperCase();
 
-    var current_contact_id = '';
-    var current_email = '';
+    let current_contact_id = '';
+    let current_email = '';
 
     AccountFactory.user.$loaded().then(() => {
 
@@ -32,100 +32,58 @@ AddController.controller('AddCtrl', ['$scope', '$location', 'ParseService', 'Acc
       if (typeof curUser.contacts !== 'object') {
 
         // First time adding contacts
-        curUser.contacts = {}
+        curUser.contacts = {};
 
         curUser.contacts[key] = {
-          first_name: first_name,0o9p0-p9o
-          last_name: last_name
+          first_name: firstName,
+          last_name: lastName
         };
 
         AccountFactory.user.$save().then((ref) => {
 
-          // TODO reroute
+          // TODO reroute to profile editing view.
         });
       } else {
 
-        // Has contacts already; check for possible duplicate
+        // Has contacts already
         let existingContacts = curUser.contacts;
-        if (contacts.hasOwnProperty(key)) {
-          console.log('Contact already exists.');
+
+        // Check for possible duplicate
+        if (existingContacts.hasOwnProperty(key)) {
+
+          // Updating existing contact, TODO add meeting
         } else {
-          console.log('New contact.');
+
+          // New contact
+          curUser.contacts[key] = {
+            first_name: firstName,
+            last_name: lastName
+          };
+
+          AccountFactory.user.$save().then((ref) => {
+
+            curUser.contacts[key].meetings = {
+              test: {
+                met_at: metAt,
+                type: type,
+                location: location
+              }
+            }
+
+            AccountFactory.user.$save().then((ref) => {
+
+              // TODO reroute to profile view
+            });
+          });
         }
       }
     });
 
-    //
-    // var authPromise = ParseService.getAllContacts();
-    //
-    // authPromise.success(function(data) {
-    //
-    //     // Used to check if current entry already exists as contact
-    //     var contacts = data.results;
-    //
-    //     var first_name = payload['first_name'];
-    //     var last_name = payload['last_name'];
-    //     var location = payload['location'];
-    //
-    //     // Parsing data correctly; this should never be an empty string
-    //     var met_at = $scope.input['met_at'];
-    //     var type = payload['type'].toUpperCase();
-    //
-    //     var matches = false;
-    //     var current_contact_id = '';
-    //     var current_email = '';
-    //
-    //     // Searching to see if contact already exists
-    //     for (var i = 0; i < contacts.length; i++) {
-    //         if (contacts[i].first_name === first_name && contacts[i].last_name === last_name) {
-    //             matches = true;
-    //             current_contact_id = contacts[i].objectId;
-    //             current_email = contacts[i].email;
-    //         };
-    //     };
-    //
-    //     // New contact
-    //     if (matches === false) {
-    //
-    //         var authPromise = ParseService.createContact({
-    //             'first_name': first_name,
-    //             'last_name': last_name
-    //         });
-    //         authPromise.success(function(data) {
-    //
-    //             // Save current contact ID
-    //             var current_contact_id = data.objectId;
-    //
-    //             // Creating meeting to be updated later
-    //             var authPromise = ParseService.createMeeting({
-    //                 'contact': {
-    //                     __type: 'Pointer',
-    //                     className: "contact",
-    //                     // Adding meeting with new user's ID
-    //                     objectId: data.objectId
-    //                 },
-    //                 'met_at': ParseService.createDate(met_at),
-    //                 'type': type,
-    //                 'location': location
-    //             }).success(function(data) {
-    //
-    //                 // Added meeting
-    //                 ParseService.current_meeting_id = data.objectId;
-    //
-    //                 // Go to profile questions; meeting will be updated later
-    //                 $location.path('tab/add/profile/' + current_contact_id + '/' + data.objectId);
-    //             }).error(function(data, status) {
-    //                 console.log(status);
-    //             });
-    //
-    //         }).error(function(data, status) {
-    //             console.log(status);
-    //         });
     //     } else {
     //         // Contact already exists
     //         ParseService.current_contact_id = current_contact_id;
     //
-    //         var authPromise = ParseService.createMeeting({
+    //         let authPromise = ParseService.createMeeting({
     //             'contact': {
     //                 __type: 'Pointer',
     //                 className: "contact",
@@ -135,9 +93,11 @@ AddController.controller('AddCtrl', ['$scope', '$location', 'ParseService', 'Acc
     //             'type': type,
     //             'location': location
     //         }).success(function(data) {
-    //             var actionItemDate = met_at.getTime() + 60 * 60 * 24 * 1000;
-    //             var newDate = new Date(actionItemDate);
-    //             var newActionItemData = {
+
+
+    //             let actionItemDate = met_at.getTime() + 60 * 60 * 24 * 1000;
+    //             let newDate = new Date(actionItemDate);
+    //             let newActionItemData = {
     //                 date: ParseService.createDate(newDate),
     //                 type: "REMINDER",
     //                 text: "You met " + payload['first_name'] + " yesterday - send a follow up!",
@@ -148,7 +108,9 @@ AddController.controller('AddCtrl', ['$scope', '$location', 'ParseService', 'Acc
     //                     "objectId": current_contact_id
     //                 }
     //             };
-    //             var createActionItemPromise = ParseService.createActionItem(newActionItemData);
+
+
+    //             let createActionItemPromise = ParseService.createActionItem(newActionItemData);
     //             createActionItemPromise.success(function(data) {
     //                 console.log("action item created with id=" + data.objectId);
     //             }).error(function(data, status, config, headers) {
